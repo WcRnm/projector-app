0820f36c746a34d1b05f898722ed8ccdcc52db1f
Rory McLeod <rmcleod@road-iq.com>
2019-10-23 12:31:23 -0700

    Android Client - Preferences (WIP) - Autoconnect, remove connect button

--- clients/android/app/src/main/java/com/wcRnm/projectorController/MainActivity.kt
+++ clients/android/app/src/main/java/com/wcRnm/projectorController/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.wcRnm.projectorController
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     private val host                            = DEFAULT_HOST
     private val port                            = DEFAULT_PORT
-    private var btnConnect : Button?            = null
     private var txtStatus  : TextView?          = null
     private var client     : Client?            = null
     private var connStatus : ConnectionStatus   = ConnectionStatus.DISCONNECTED
@@ -24,8 +24,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        this.btnConnect = findViewById(R.id.button_connect)
         this.txtStatus = findViewById(R.id.textview_connectionStatus)
+
+        onConnectionStatus(ConnectionStatus.DISCONNECTED)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -34,13 +35,13 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.action_settings    -> {
+            startActivity(Intent(this, SettingsActivity::class.java))
+            true
+        }
+        else                    -> {
+            super.onOptionsItemSelected(item)
         }
     }
 
@@ -61,31 +62,26 @@ class MainActivity : AppCompatActivity() {
         when (status) {
             ConnectionStatus.DISCONNECTED  -> {
                 this.setUiText(this.txtStatus, R.string.status_disconnected)
-                this.setUiText(btnConnect, R.string.btn_connect)
-                this.enableUiItem(btnConnect, true)
+
+                Handler().postDelayed(
+                    {
+                        client = Client(host, port, ::onConnectionStatus)
+                        client?.connect()
+                    }, 1000)
             }
             ConnectionStatus.CONNECTED     -> {
                 this.setUiText(this.txtStatus, R.string.status_connected)
-                this.setUiText(btnConnect, R.string.btn_disconnect)
-                this.enableUiItem(btnConnect, true)
             }
             ConnectionStatus.CONNECTING    -> {
                 this.setUiText(this.txtStatus, R.string.status_connecting)
-                this.setUiText(btnConnect, R.string.btn_disconnect)
-                this.enableUiItem(btnConnect, false)
             }
             ConnectionStatus.DISCONNECTING -> {
                 this.setUiText(this.txtStatus, R.string.status_disconnecting)
-                this.setUiText(btnConnect, R.string.btn_disconnect)
-                this.enableUiItem(btnConnect, false)
             }
         }
     }
 
     private fun setUiText(textView: TextView?, resId: Int) {
         textView?.post(java.lang.Runnable { textView.setText(resId) })
-    }
-    private fun enableUiItem(button: Button?, enable: Boolean) {
-        button?.post(java.lang.Runnable { button.setEnabled(enable) })
     }
 }
---
+++ clients/android/app/src/main/java/com/wcRnm/projectorController/SettingsActivity.kt
@@ -0,0 +1,36 @@
+package com.wcRnm.projectorController
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.wcRnm.projectorController.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // User chose the "Settings" item, show the app settings UI...
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+}
