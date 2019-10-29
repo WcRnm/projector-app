53a97ef8a84837b929e3272eb686a9b7a79326fe
Rory McLeod <rmcleod@road-iq.com>
2019-10-29 11:44:27 -0700

    idle tasks. move some code

--- clients/android/app/src/main/java/com/wcRnm/projectorController/Projector.kt
+++ clients/android/app/src/main/java/com/wcRnm/projectorController/Projector.kt
@@ -2,6 +2,7 @@ package com.wcRnm.projectorController
 
 import android.util.Log
 import java.io.OutputStream
+import java.util.*
 
 private const val TAG: String = "Projector"
 
@@ -16,63 +17,24 @@ interface IProjector {
     fun idleTasks(outputStream: OutputStream)
 }
 
-const val CMD_NONE          = 0
-const val CMD_POWER_ON      = 1
-const val CMD_POWER_OFF     = 2
-const val CMD_VOLUME_UP     = 3
-const val CMD_VOLUME_DOWN   = 4
-const val CMD_MUTE          = 5
-const val CMD_MENU_UP       = 6
-const val CMD_MENU_DOWN     = 7
-const val CMD_MENU_LEFT     = 8
-const val CMD_MENU_RIGHT    = 9
-const val CMD_MENU          = 10
-const val CMD_MENU_ENTER    = 11
-const val CMD_MENU_EXIT     = 12
-const val CMD_HELP          = 13
-const val CMD_VOLUME        = 14
-const val CMD_WARMING_UP    = 15
-const val CMD_WARMING_VAL   = 16
-const val CMD_HELP_MSG      = 17
-const val CMD_JUMP_TO_TOOLS = 18
-const val CMD_JUMP_TO_INFO  = 19
-const val CMD_CS_IP_ADDR    = 20
-const val CMD_CS_IP_ID      = 21
-const val CMD_CS_PORT       = 22
-const val CMD_PROJECTOR_NAME = 23
-const val CMD_LOCATION      = 24
-const val CMD_IP_ADDR       = 25
-const val CMD_SUBNET_MASK   = 26
-const val CMD_DEFAULT_GATEWAY = 27
-const val CMD_DNS_SERVER    = 28
-const val CMD_USER_NAME     = 29
-const val CMD_USER_PWRD         = 30
-const val CMD_USER_CONFIRM      = 31
-const val CMD_ADMIN_PWRD        = 32
-const val CMD_ADMIN_CONFIRM     = 33
-const val CMD_JUMP_TO_HELP      = 34
-const val CMD_U_PWRD_ENABLED    = 35
-const val CMD_A_PWRD_ENABLED    = 36
-const val CMD_DHCP_ENABLED      = 37
-const val CMD_FIRMWARE      = 38
-const val CMD_MAC_ADDR      = 39
-const val CMD_RESOLUTION    = 40
-const val CMD_LAMP_HOURS    = 41
-const val CMD_ASSIGNED_TO   = 42
-const val CMD_POWER_STATUS  = 43
-const val CMD_PRESET_MODE   = 44
-const val CMD_PROJ_POS      = 45
-const val CMD_LAMP_MODE     = 46
-const val CMD_ERROR_STATUS  = 47
-const val CMD_KEYPAD_EXT1   = 48
-const val CMD_KEYPAD_EXT2   = 49
-const val CMD_KEYPAD_EXT3   = 50
-const val CMD_KEYPAD_EXT4   = 51
-const val CMD_MUTE_OFF      = 52
-const val CMD_KEYPAD_EXT1_T = 53
-const val CMD_KEYPAD_EXT2_T = 54
-const val CMD_KEYPAD_EXT3_T = 55
-const val CMD_KEYPAD_EXT4_T = 56
+enum class ProjectorTask { SEND_CONNECT_MSG, REQUEST_INFO, DIGITAL_VALUE, ANALOG_VALUE, SERIAL_VALUE }
+class ProjectorTaskItem(val task: ProjectorTask, val id: Int, val bVal: Boolean, val iVal: Int, val sVal: String) {
+
+    constructor(task: ProjectorTask): this(task, 0, false, 0, "")
+    constructor(task: ProjectorTask, id: Int, bVal: Boolean): this(task, id, bVal, 0, "")
+    constructor(task: ProjectorTask, id: Int, iVal: Int): this(task, id, false, iVal, "")
+    constructor(task: ProjectorTask, id: Int, sVal: String): this(task, id, false, 0, sVal)
+
+    override fun toString(): String {
+        return when (task) {
+            ProjectorTask.SEND_CONNECT_MSG  -> "$task"
+            ProjectorTask.REQUEST_INFO      -> "$task"
+            ProjectorTask.DIGITAL_VALUE     -> "$task ${id}:${bVal}"
+            ProjectorTask.ANALOG_VALUE      -> "$task ${id}:${iVal}"
+            ProjectorTask.SERIAL_VALUE      -> "$task ${id}:${sVal}"
+        }
+    }
+}
 
 class InfocusIN2128HDx : IProjector {
     private var handle: Int = DEFAULT_HANDLE  // projector ID
@@ -82,106 +44,61 @@ class InfocusIN2128HDx : IProjector {
     private var supportsRepeatDigitals  = false
     private var supportsHeartbeat       = false
 
-    private var idleTicks = System.currentTimeMillis()
-    private var buffer    = ByteArray(0)
+    private var idleTicks   = System.currentTimeMillis()
+    private var buffer      = ByteArray(0)
+    private val taskQ       = ArrayDeque<ProjectorTaskItem>()
+    private val props       = ProjectorProperties()
 
     override fun idleTasks(outputStream: OutputStream) {
-        if (cnxConnected && supportsHeartbeat) {
-            val now = System.currentTimeMillis()
-            if ((now - idleTicks) > HEARTBEAT_INTERVAL) {
-                outputStream.write(heartbeat)
-                idleTicks = System.currentTimeMillis()
+        //Log.d(TAG, "++idleTasks(${cnxConnected}, n:${taskQ.size})")
+        //if (cnxConnected) {
+
+            while (taskQ.size > 0) {
+                val task = taskQ.removeFirst()
+
+                Log.d(TAG, task.toString())
+
+                when (task.task) {
+                    ProjectorTask.SEND_CONNECT_MSG -> {
+                        outputStream.write(msgConnect(ipid))
+                    }
+                    ProjectorTask.REQUEST_INFO     -> {
+                        outputStream.write(msgUpdateRequest(handle))
+                        //outputStream.write(msgDigitalCommand(handle, CMD_CS_PORT))
+                    }
+                    ProjectorTask.DIGITAL_VALUE     -> props.set(task.id, task.bVal)
+                    ProjectorTask.ANALOG_VALUE      -> props.set(task.id, task.iVal)
+                    ProjectorTask.SERIAL_VALUE      -> props.set(task.id, task.sVal)
+                }
             }
-        }
+
+            if (supportsHeartbeat) {
+                val now = System.currentTimeMillis()
+                if ((now - idleTicks) > HEARTBEAT_INTERVAL) {
+                    outputStream.write(msgHeartbeat(handle))
+                    idleTicks = System.currentTimeMillis()
+                }
+            }
+        //}
+        //else {
+        //    if (taskQ.size > 0)
+        //    taskQ.clear()
+        //}
+        //Log.d(TAG, "--idleTasks")
     }
-
-    private val heartbeat = byteArrayOfInts(
-        13,
-        0,
-        2,
-        handle / 256,
-        handle % 256
-    )
-    private val connectMessage = byteArrayOfInts(
-        1,
-        0,
-        7,
-        0,
-        0,
-        0,
-        0,
-        ipid / 256,
-        ipid % 256,
-        64
-    )
-
-    private val endOfQueryResponse = byteArrayOfInts(
-        5,
-        0,
-        5,
-        handle / 256,
-        handle % 256,
-        2,
-        3,
-        29
-    )
-
-    private val updateRequestPacket = byteArrayOfInts(
-        5,
-        0,
-        5,
-        handle / 256,
-        handle % 256,
-        2,
-        3,
-        30
-    )
-
-    fun digitalCommand(stdCmd:Int, value:Boolean, extraParam:Int) : ByteArray
-    {
-        val extraLen:Byte = if (extraParam > 0) 3 else 0
-        val iCmd = stdCmd-1
-
-        var cmd = byteArrayOfInts(
-            5,
-            0,
-            6 + extraLen,
-            handle / 256,
-            handle % 256,
-            3 + extraLen
-        )
-
-        if(extraParam > 0)
-        {
-            cmd += 32
-            cmd += extraParam.toByte()
-            cmd += 3
-        }
-
-        cmd += 0
-        cmd += (iCmd % 256).toByte()
-
-        when (value) {
-            false -> cmd += ((iCmd / 256) or 128).toByte()
-            true  -> cmd += (iCmd / 256).toByte()
-        }
-
-        return cmd
-    }
-
-    private fun byteArrayOfInts(vararg ints: Int) = ByteArray(ints.size) { pos -> ints[pos].toByte() }
 
     override fun handleData(data: ByteArray, dataLen: Int, outStream: OutputStream) {
-        Log.d(TAG, "++handleData(${dataLen})")
+        //Log.d(TAG, "++handleData(${dataLen})")
         buffer += data.take(dataLen)
-        val len = nextPacketLength(buffer)
+        var len = nextPacketLength(buffer)
 
-        if (len > 0) {
+        while (len > 0) {
             val packet = buffer.take(len).toByteArray()
             buffer = buffer.drop(len).toByteArray()
             handlePacket(packet, outStream)
+            len = nextPacketLength(buffer)
         }
-        Log.d(TAG, "--handleData")
+        //Log.d(TAG, "--handleData")
     }
 
     private fun nextPacketLength(avail: ByteArray) : Int {
@@ -199,31 +116,35 @@ class InfocusIN2128HDx : IProjector {
     private fun handlePacket(packet: ByteArray, outputStream: OutputStream) {
         val msgId = packet[0].toInt()
 
-        Log.d(TAG, "++handlePacket(id:${msgId} len:${packet.size})")
+        //Log.d(TAG, "++handlePacket(id:${msgId} len:${packet.size})")
 
         when (msgId) {
-            2    -> handleConnect(packet, outputStream)
+            2    -> handleConnectResponse(packet)
             3    -> handleDisconnect()
             4    -> handleDisconnect()
             5    -> handleDataPacket(packet, outputStream)
             14   -> handleHeartbeat()
-            15   -> {
-                if (packet.size > 3) {
-                    when(packet[3].toInt()) {
-                        0 -> handleDisconnect()
-                        2 -> {
-                            if (!cnxConnected)
-                                outputStream.write(connectMessage)
-                        }
-                    }
-                }
-            }
+            15   -> handleConnectStatus(packet)
             else -> { /* ignore */ }
         }
-        Log.d(TAG, "--handlePacket")
+        //Log.d(TAG, "--handlePacket")
     }
 
-    private fun handleConnect(data:ByteArray, outputStream: OutputStream) {
+    private fun handleConnectStatus(packet:ByteArray) {
+        if (packet.size > 3) {
+            Log.d(TAG, "++handleConnectStatus(${packet[3]}, connected=${cnxConnected})")
+            when(packet[3].toInt()) {
+                0 -> handleDisconnect()
+                2 -> {
+                    if (!cnxConnected)
+                        taskQ.add(ProjectorTaskItem(ProjectorTask.SEND_CONNECT_MSG))
+                }
+            }
+        }
+        Log.d(TAG, "--handleConnectStatus()")
+    }
+
+    private fun handleConnectResponse(data:ByteArray) {
         Log.d(TAG, "++handleConnect")
 
         if (cnxConnected)
@@ -241,10 +162,14 @@ class InfocusIN2128HDx : IProjector {
             Log.d(TAG, "handleConnect: supports heartbeat")
         }
 
-        outputStream.write(updateRequestPacket)
+        // this seems redundant, we already sent this message!
+        //if (!cnxConnected)
+        //    taskQ.add(ProjectorTaskItem(ProjectorTask.SEND_CONNECT_MSG))
 
         idleTicks    = System.currentTimeMillis()
         cnxConnected = true
+
+        taskQ.add(ProjectorTaskItem(ProjectorTask.REQUEST_INFO))
 
         Log.d(TAG, "--handleConnect")
     }
@@ -263,25 +188,23 @@ class InfocusIN2128HDx : IProjector {
     }
 
     private fun handleDigitalPacket(data:ByteArray, offset:Int) {
-        Log.d(TAG, "handleDigitalPacket")
+        //Log.d(TAG, "handleDigitalPacket")
 
         if(data[5 + offset].toInt() != 3) {
             return
         }
         var id = data[8 + offset] * 256 + data[7 + offset]
-        id = (id and 32767) + 1
-        val stateByte = data[8 + offset].toInt()
-        var stateBool = ((stateByte and 128) != 128)
+        id = (id and 32767) + 1 // (id & 0x7FFF) + 1
+        val bool = ((data[8 + offset].toInt() and 128) != 128) // 0x80
 
-        //val event = new DigitalEvent(CNXConnection.DIGITAL,id,stateBool)
-        //dispatchEvent(event)
+        taskQ.add(ProjectorTaskItem(ProjectorTask.DIGITAL_VALUE, id, bool))
     }
 
     private fun handleAnalogPacket(data:ByteArray, offset:Int) {
-        Log.d(TAG, "handleAnalogPacket")
+        //Log.d(TAG, "handleAnalogPacket")
 
         var id = data[7 + offset].toInt() + 1
-        var value: Int
+        val value: Int
         when(data[5 + offset].toInt()) {
             3 -> value = data[8 + offset].toInt()
             4 -> value = data[8 + offset] * 256 + data[9 + offset]
@@ -291,12 +214,11 @@ class InfocusIN2128HDx : IProjector {
             }
             else -> return
         }
-        //eventAnalog = new AnalogEvent(CNXConnection.ANALOG, id, value)
-        //dispatchEvent(eventAnalog)
+        taskQ.add(ProjectorTaskItem(ProjectorTask.ANALOG_VALUE, id, value))
     }
 
     private fun handleSerialPacket1(data:ByteArray, offset:Int, type:Int) {
-        Log.d(TAG, "handleSerialPacket1")
+        //Log.d(TAG, "handleSerialPacket1")
 
         var msg = ""
         val id: Int = data[7 + offset] * 256 + data[8 + offset] + 1
@@ -307,12 +229,12 @@ class InfocusIN2128HDx : IProjector {
         while(i++ < n) {
             msg += data[j++].toChar()
         }
-        //eventSerial = new SerialEvent(CNXConnection.SERIAL, id, msg)
-        //dispatchEvent(eventSerial)
+
+        taskQ.add(ProjectorTaskItem(ProjectorTask.SERIAL_VALUE, id, msg))
     }
 
     private fun handleSerialPacket2(data:ByteArray, offset:Int, type:Int) {
-        Log.d(TAG, "handleSerialPacket2")
+        //Log.d(TAG, "handleSerialPacket2")
 
         var msg = ""
         val id = data[7 + offset].toInt() + 1
@@ -322,8 +244,8 @@ class InfocusIN2128HDx : IProjector {
         while(i++ < n) {
             msg += data[j++].toChar()
         }
-        //eventSerial = new SerialEvent(CNXConnection.SERIAL, id, msg)
-        //dispatchEvent(eventSerial)
+
+        taskQ.add(ProjectorTaskItem(ProjectorTask.SERIAL_VALUE, id, msg))
     }
 
     private fun handleSerialPacket3(data:ByteArray, offset:Int, type:Int) {
@@ -342,28 +264,27 @@ class InfocusIN2128HDx : IProjector {
                 while(i < j && data[i].toInt() != 13) {
                     msg += msg + data[i++].toChar()
                 }
-                //eventSerial = new SerialEvent(CNXConnection.SERIAL,id, msg)
-                //dispatchEvent(eventSerial)
-                i++
-                i++
+
+                taskQ.add(ProjectorTaskItem(ProjectorTask.SERIAL_VALUE, id, msg))
+
+                i += 2
             }
         }
     }
 
-    private fun handleEndOfQueryPacket(data:ByteArray, offset:Int) {
+    private fun handleEndOfQueryPacket(data:ByteArray, offset:Int, os: OutputStream) {
         Log.d(TAG, "handleEndOfQueryPacket")
 
-        val i = data[7 + offset].toInt()
-        if (i == 0 || i == 31) {
-            //val event = ClearAllEvent(CNXConnection.ALLCLEAR);
-            //dispatchEvent(event)
-        } else {
-            //SendEndOfQueryResponse()
-        }
+        //val i = data[7 + offset].toInt()
+        //if (i == 0 || i == 31) {
+        //    //val event = ClearAllEvent(CNXConnection.ALLCLEAR);
+        //    //dispatchEvent(event)
+        //}
+        os.write(msgEndOfQueryResponse(handle))
     }
 
-    private fun handleDataPacket(data: ByteArray, outputStream: OutputStream) {
-        Log.d(TAG, "handleDataPacket")
+    private fun handleDataPacket(data: ByteArray, os: OutputStream) {
+        //Log.d(TAG, "handleDataPacket")
 
         var offset = 0
         var extraData = 0
@@ -383,7 +304,7 @@ class InfocusIN2128HDx : IProjector {
                 21 -> handleSerialPacket1(data, offset, 0)
                 18 -> handleSerialPacket2(data, offset, 1)
                 2  -> handleSerialPacket3(data, offset, 2)
-                3  -> handleEndOfQueryPacket(data, offset)
+                3  -> handleEndOfQueryPacket(data, offset, os)
             }
         }
     }
