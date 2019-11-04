0c179a2f16f92fc5e1adfbab3fd5aebbf64227eb
Rory McLeod <mcleodnw@gmail.com>
2019-11-03 17:23:09 -0800

    display all projector properties on the webpage.

--- server/cherrypy/projector_info.py
+++ server/cherrypy/projector_info.py
@@ -1,31 +1,28 @@
 # Statuses
-ONLINE = 'online'
-LOCATION = 'location'
-LAMP_HRS = 'lampHrs'
-SERVICE_HRS = 'serviceHrs'
-MSG_COUNT = 'msgCount'
+ONLINE = 'conn-online'
+MSG_COUNT = 'conn-msg-count'
 
 DATA_TEXT = 'text'
-DATA_BOOL = 'bool'
+DATA_BOOL = 'boolean'
 DATA_ANALOG = 'analog'
 
 TEXT_NAMES = {
-    2: 'error',
-    5: LAMP_HRS,
-    5003: 'mode',
-    5040: 'ip-addr',
-    5041: 'netmask',
-    5044: 'mac-addr',
-    5047: 'port',
-    5052: LOCATION,
-    5054: 'resolution',
-    5056: 'firmware-version',
-    5070: 'source-1',
-    5071: 'source-2',
-    5072: 'source-3',
-    5073: 'source-4',
-    5074: 'source-5',
-    5075: 'source-6',
+    2: 'state-error',
+    5: 'state-lamp-hrs',
+    5003: 'state-mode',
+    5040: 'network-ip-addr',
+    5041: 'network-netmask',
+    5044: 'network-mac-addr',
+    5047: 'network-port',
+    5052: 'config-location',
+    5054: 'info-resolution',
+    5056: 'info-firmware-version',
+    5070: 'value-source-1',
+    5071: 'value-source-2',
+    5072: 'value-source-3',
+    5073: 'value-source-4',
+    5074: 'value-source-5',
+    5075: 'value-source-6',
 }
 
 BOOL_NAMES = {
@@ -40,17 +37,17 @@ ANALOG_NAMES = {
 def get_id_name(data_id, data_type):
     if DATA_ANALOG == data_type:
         table = ANALOG_NAMES
-        prefix = 'A'
+        prefix = 'x-analog-'
     elif DATA_BOOL == data_type:
         table = BOOL_NAMES
-        prefix = 'B'
+        prefix = 'x-bool-'
     elif DATA_TEXT == data_type:
         table = TEXT_NAMES
-        prefix = 'T'
+        prefix = 'x-text-'
     else:
         return None
 
     try:
         return table[data_id]
     except Exception as e:
-        return '{}-{}'.format(prefix, data_id)
+        return '{}{}'.format(prefix, data_id)
--- server/cherrypy/static/js/projector.js
+++ server/cherrypy/static/js/projector.js
@@ -1,23 +1,72 @@
+let LOCATION = 'config-location'
 
+var initialized = false
 
 function setStatus(status) {
-    document.getElementById("location0").innerHTML = status[0].location;
-    document.getElementById("online0").innerHTML = status[0].online;
-    document.getElementById("lampHrs0").innerHTML = status[0].lampHrs;
-    document.getElementById("serviceHrs0").innerHTML = status[0].serviceHrs;
-    document.getElementById("msgCount0").innerHTML = status[0].msgCount;
+  table = document.getElementById("proj-table").getElementsByTagName('tbody')[0];
+  nproj = status.length;
+
+  for (var i=0; i<nproj; ++i) {
+    let projector = status[0];
+
+    // add the column header
+    var row = document.getElementById("proj-header");
+    var cell;
+    
+    var cell_id = "proj-name" + i;
+    cell = document.getElementById(cell_id);
+    if (cell == null) {
+      cell = document.createElement('th');
+      cell.id = "proj-name" + i;
+      row.appendChild(cell);
+    }
+    cell.innerHTML = projector[LOCATION];
+
+    Object.keys(projector)
+      .sort()
+      .forEach(function(prop, i) {
+          let row_id = "prop-" + prop;
+          row = document.getElementById(row_id);
+  
+          if (null == row) {
+            row = table.insertRow();
+            row.id = row_id;
+  
+            cell = row.insertCell();
+            cell.id = row_id + "-name";
+            cell.innerHTML = prop.toString();
+  
+            //for (var i=0; i<nproj; ++i) {
+            //}
+          }
+
+          cell_id = row_id + "i";
+          cell = document.getElementById(cell_id);
+          if (cell == null) {
+            cell = row.insertCell();
+            cell.id = row_id + "i";
+          }
+          cell.innerHTML = projector[prop];
+        });
+  }
 }
 
 function getStatus() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        setStatus(JSON.parse(this.responseText))
+    if (this.readyState == 4) {
+
+      switch(this.status) {
+        case 200:
+          setStatus(JSON.parse(this.responseText));
+          break;
+        default: 
+          break;
+      }
     }
   };
   xhttp.open("GET", "projector", true);
   xhttp.send();
 }
 
-
-getStatus()
+getStatus();
